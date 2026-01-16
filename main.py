e145dc6e3e3e7f7c00a131ef3cdb500deb8ca33d
MingxuanGame <MingxuanGame@outlook.com>
2026-01-16 19:40:26 +0800

    Add community client directory and generating

--- main.py
+++ main.py
@@ -13,7 +13,10 @@ from stream_unzip import async_stream_unzip
 
 GITHUB_API_RELEASES_API = "https://api.github.com/repos/{owner}/{repo}/releases"
 
-CLIENTS_FILE = "clients.json"
+DEFAULT_CLIENTS_FILE = Path("default_clients.json")
+COMMUNITY_CLIENTS_PATH = Path("community_clients")
+DEFAULT_VERSION_LIST_OUTPUT = Path("version_list.json")
+COMMUNITY_VERSION_LIST_OUTPUT = Path("community_versions")
 
 # client
 
@@ -35,8 +38,10 @@ class Client(TypedDict):
     support_ios: bool
 
 
-def read_clients() -> list[Client]:
-    clients = json.loads(Path(CLIENTS_FILE).read_text())
+def read_default_clients() -> list[Client]:
+    clients = json.loads(DEFAULT_CLIENTS_FILE.read_text())
+
+    # set defaults
     for client in clients:
         if "count" not in client:
             client["count"] = 1
@@ -45,6 +50,24 @@ def read_clients() -> list[Client]:
         if "support_ios" not in client:
             client["support_ios"] = True
     return clients
+
+
+def read_community_clients() -> dict[str, list[Client]]:
+    communities: dict[str, list[Client]] = {}
+    for community_file in COMMUNITY_CLIENTS_PATH.glob("*.json"):
+        clients = json.loads(community_file.read_text())
+
+        # set defaults
+        for client in clients:
+            if "count" not in client:
+                client["count"] = 1
+            if "support_android" not in client:
+                client["support_android"] = True
+            if "support_ios" not in client:
+                client["support_ios"] = True
+
+        communities[community_file.name] = clients
+    return communities
 
 
 # github releases
@@ -211,92 +234,129 @@ class VersionList(TypedDict):
     versions: list[VersionInfo]
 
 
-async def main(gh_token: str = ""):
-    clients = read_clients()
-    version_list: list[VersionList] = []
-    for i, client in enumerate(clients):
-        print(f"--- ({i + 1}/{len(clients)}) Generating version for ---")
-        print(f"\tClient: {client['name']}")
-        print(f"\tDescription: {client['description']}")
-        print(f"\tRepository: {client['owner']}/{client['repo']}")
+async def generate_versions(client: Client, gh_token: str) -> list[VersionInfo]:
+    versions: list[VersionInfo] = []
+    releases = await fetch_releases(client["owner"], client["repo"], client["count"], authorization=gh_token)
 
-        versions: list[VersionInfo] = []
-        releases = await fetch_releases(client["owner"], client["repo"], client["count"], authorization=gh_token)
-        for release in releases:
-            version_hashes: dict[str, str] = {}
-            print(f"\t  Release: {release['release_name']} ({release['tag']})")
-            tag = release["tag"].removeprefix("v")
+    for release in releases:
+        version_hashes: dict[str, str] = {}
+        print(f"\t  Release: {release['release_name']} ({release['tag']})")
+        tag = release["tag"].removeprefix("v")
 
-            def compute_md5(s: str) -> str:
-                md5 = hashlib.md5(usedforsecurity=False)
-                md5.update(s.encode("utf-8"))
-                return md5.hexdigest()
+        def compute_md5(s: str) -> str:
+            md5 = hashlib.md5(usedforsecurity=False)
+            md5.update(s.encode("utf-8"))
+            return md5.hexdigest()
 
-            async def process_one(osname, file_info):
-                asset_name = file_info["asset_name"]
-                internal_name = file_info["internal_name"]
+        async def process_one(osname, file_info):
+            asset_name = file_info["asset_name"]
+            internal_name = file_info["internal_name"]
 
-                 # replace placeholders
-                asset_name = asset_name.replace("{tag}", release["tag"])
+            # replace placeholders
+            asset_name = asset_name.replace("{tag}", release["tag"])
 
-                if asset_name not in release["files"]:
-                    print(f"\t    [!] Asset '{asset_name}' not found in release.")
-                    return None
-
-                file_url = release["files"][asset_name]
-                type_ = file_info.get("type", "zip")
-                processor = PROCESSORS.get(type_)
-                if not processor:
-                    print(f"\t    [!] No processor found for type '{type_}'.")
-                    return None
-
-                print(f"\t    Processing asset: {asset_name}")
-                try:
-                    version_hash = await processor(file_url, internal_name)
-                    if version_hash:
-                        print(f"\t      {osname} version hash: {version_hash}")
-                        return version_hash, osname
-                    else:
-                        print(f"\t      [!] Internal file '{internal_name}' not found in asset.")
-                except Exception as e:
-                    print(f"\t      [!] Error processing file: {e}")
-                    traceback.print_exc()
+            if asset_name not in release["files"]:
+                print(f"\t    [!] Asset '{asset_name}' not found in release.")
                 return None
 
-            results = await asyncio.gather(
-                *(process_one(osname, file_info) for osname, file_info in client["files"].items())
-            )
-            for res in results:
-                if res:
-                    version_hashes[res[0]] = res[1]
+            file_url = release["files"][asset_name]
+            type_ = file_info.get("type", "zip")
+            processor = PROCESSORS.get(type_)
+            if not processor:
+                print(f"\t    [!] No processor found for type '{type_}'.")
+                return None
 
-            # android and ios
-            # https://github.com/ppy/osu/blob/master/osu.Game/OsuGameBase.cs#L270-L275
-            if client["support_android"]:
-                version_str = f"{tag}-Android"
-                hash = compute_md5(version_str)
-                version_hashes[hash] = "Android"
-                print(f"\t      Android version hash: {hash}")
-            if client["support_ios"]:
-                version_str = f"{tag}-iOS"
-                hash = compute_md5(version_str)
-                version_hashes[hash] = "iOS"
-                print(f"\t      iOS version hash: {hash}")
+            print(f"\t    Processing asset: {asset_name}")
+            try:
+                version_hash = await processor(file_url, internal_name)
+                if version_hash:
+                    print(f"\t      {osname} version hash: {version_hash}")
+                    return version_hash, osname
+                else:
+                    print(f"\t      [!] Internal file '{internal_name}' not found in asset.")
+            except Exception as e:
+                print(f"\t      [!] Error processing file: {e}")
+                traceback.print_exc()
+            return None
 
-            version_info: VersionInfo = {
-                "version": tag,
-                "release_date": release["date"],
-                "hashes": version_hashes,
-            }
-            versions.append(version_info)
-        version_list.append(
-            {
-                "name": client["name"],
-                "versions": versions,
-            }
+        results = await asyncio.gather(
+            *(process_one(osname, file_info) for osname, file_info in client["files"].items())
         )
-    print("--- Version list generated, output to version_list.json ---")
-    Path("version_list.json").write_text(json.dumps(version_list, indent=4))
+        for res in results:
+            if res:
+                version_hashes[res[0]] = res[1]
+
+        # android and ios
+        # https://github.com/ppy/osu/blob/master/osu.Game/OsuGameBase.cs#L270-L275
+        if client["support_android"]:
+            version_str = f"{tag}-Android"
+            hash = compute_md5(version_str)
+            version_hashes[hash] = "Android"
+            print(f"\t      Android version hash: {hash}")
+        if client["support_ios"]:
+            version_str = f"{tag}-iOS"
+            hash = compute_md5(version_str)
+            version_hashes[hash] = "iOS"
+            print(f"\t      iOS version hash: {hash}")
+
+        version_info: VersionInfo = {
+            "version": tag,
+            "release_date": release["date"],
+            "hashes": version_hashes,
+        }
+        versions.append(version_info)
+    return versions
+
+
+async def main(gh_token: str = "", skip_default: bool = False, skip_community: bool = False):
+    clients = read_default_clients()
+    version_list: list[VersionList] = []
+
+    if not skip_default:
+        print("=== Generating version list for default clients ===")
+        for i, client in enumerate(clients):
+            print(f"--- ({i + 1}/{len(clients)}) Generating version for default clients ---")
+            print(f"\tClient: {client['name']}")
+            print(f"\tDescription: {client['description']}")
+            print(f"\tRepository: {client['owner']}/{client['repo']}")
+
+            versions = await generate_versions(client, gh_token)
+            version_list.append(
+                {
+                    "name": client["name"],
+                    "versions": versions,
+                }
+            )
+        print("--- Default version list generated, output to version_list.json ---")
+        DEFAULT_VERSION_LIST_OUTPUT.write_text(json.dumps(version_list, indent=4))
+
+    if not skip_community:
+        print("=== Generating version list for community clients ===")
+        communities = read_community_clients()
+        i = 1
+        for community_filename, community_clients in communities.items():
+            version_list = []
+            print(f"--- ({i}/{len(communities)}) Generating version for community client from {community_filename} ---")
+            for j, cc in enumerate(community_clients):
+                print(f"~~~ ({j + 1}/{len(community_clients)}) ~~~")
+                print(f"\tClient: {cc['name']}")
+                print(f"\tDescription: {cc['description']}")
+                print(f"\tRepository: {cc['owner']}/{cc['repo']}")
+
+                versions = await generate_versions(cc, gh_token)
+                version_list.append(
+                    {
+                        "name": cc["name"],
+                        "versions": versions,
+                    }
+                )
+
+                i += 1
+
+                COMMUNITY_VERSION_LIST_OUTPUT.mkdir(exist_ok=True)
+            output_path = COMMUNITY_VERSION_LIST_OUTPUT / community_filename
+            output_path.write_text(json.dumps(version_list, indent=4))
+            print(f"--- Community {community_filename} version list generated, output to {output_path} ---")
 
 
 if __name__ == "__main__":
@@ -307,4 +367,6 @@ if __name__ == "__main__":
         print("This script currently only supports Linux platform.")
         sys.exit(1)
 
-    asyncio.run(main(os.environ.get("GH_TOKEN", "")))
+    skip_default = "--skip-default" in sys.argv
+    skip_community = "--skip-community" in sys.argv
+    asyncio.run(main(os.environ.get("GH_TOKEN", ""), skip_default=skip_default, skip_community=skip_community))
